9f8f5205cfabf20b83f16fc7576925af16c5ed30
Aitsuken <enderlordschannel@gmail.com>
2022-05-22 18:04:02 +0600

    Initial D

---
+++ src/main/java/kg/kuraido/mywaifucloud/MyWaifuCloudApplication.java
@@ -0,0 +1,13 @@
+package kg.kuraido.mywaifucloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyWaifuCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWaifuCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/kuraido/mywaifucloud/MyWaifuCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kuraido.mywaifucloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyWaifuCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
